ea177a99a7a7df256de0805cb2cadd5e5416f99c
Rodney Madondo <madondotakunda09@gmail.com>
2024-07-25 16:20:58 +0200

    fixed the carousel

--- index.js
+++ index.js
@@ -7,12 +7,42 @@ hamburgerBtn.addEventListener('click', () => {
     links.classList.toggle('show-links');
     closeBtn.classList.toggle('hidden');
     hamburgerBtn.classList.toggle('hidden');
-    overlay.classList.remove('hidden')
+    overlay.classList.remove('hidden');
 });
 
 closeBtn.addEventListener('click', ()=>{
     links.classList.toggle("show-links");
-    closeBtn.classList.toggle('hidden')
+    closeBtn.classList.toggle('hidden');
     hamburgerBtn.classList.remove('hidden');
-    overlay.classList.add('hidden')
+    overlay.classList.add('hidden');
 })  
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    direction: 'horizontal',
+    loop: true,
+    breakpoints: {
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      680:{
+        slidesPerView:2,
+        spaceBetween:30
+      },
+      900 :{
+        slidesPerView:3,
+        spaceBetween:30
+      }
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+  
+  });
